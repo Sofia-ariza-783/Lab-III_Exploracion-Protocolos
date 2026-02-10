3a22d08ce8181bcca2508c92b742ed0f46fe14a0
Komi_yes <danielpalacios04@gmail.com>
2026-02-10 18:13:33 -0500

    feat(GUI): Implement basic browser functionality
    Refactor browser GUI to allow URL input and display
    Add action listener for 'Go' button to load URLs
    Update main to launch browser and load a default URL

--- src/main/java/co/eci/protocols/GUI/browser.java
+++ src/main/java/co/eci/protocols/GUI/browser.java
@@ -2,24 +2,48 @@ package co.eci.protocols.GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+
+import static co.eci.protocols.exercices.URLInfo.GenerateURLInfo;
 
 public class browser extends JFrame {
     private JTextField urlField;
     private JButton goButton;
     private JEditorPane displayArea;
+    private String url;
+
     public browser() {
         setTitle("Browser");
-        setSize(400, 300);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize.width/2, screenSize.height/2);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
 
-        urlField = new JTextField(20);
+        urlField = new JTextField(50);
         goButton = new JButton("Go");
         displayArea = new JEditorPane();
 
+        panel.add(urlField);
+        panel.add(goButton);
+        add(panel, BorderLayout.NORTH);
+        add(new JScrollPane(displayArea), BorderLayout.CENTER);
+        
+        goButton.addActionListener(e -> {
+            url = urlField.getText();
+            try {
+                GenerateURLInfo(url);
+                displayArea.setPage("Archivo generado");
+
+            } catch (IOException ex) {
+                displayArea.setText("Error loading URL: " + ex.getMessage());
+            }
+        });
+
     }
+
+
 
 }
--- src/main/java/co/eci/protocols/main.java
+++ src/main/java/co/eci/protocols/main.java
@@ -1,14 +1,15 @@
 package co.eci.protocols;
 
 import co.eci.protocols.GUI.browser;
-import co.eci.protocols.exercices.URLInfo;
+
+import static co.eci.protocols.exercices.URLInfo.GenerateURLInfo;
 
 
 public class main {
     public static void main(String[] args) {
         browser browser = new browser();
         browser.setVisible(true);
-        URLInfo.GenerateURLInfo("https://docs.oracle.com/javase/tutorial/networking/index.html");
+        GenerateURLInfo("https://docs.oracle.com/javase/tutorial/networking/index.html");
     }
 
 }
