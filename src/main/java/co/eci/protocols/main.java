98382aea232a95a9db43b8467abcf8b2b3440b7d
sofia.ariza-g <sofitet783@gmail.com>
2026-02-10 17:43:01 -0500

    feat(): fix main

--- src/main/java/co/eci/protocols/main.java
+++ src/main/java/co/eci/protocols/main.java
@@ -3,7 +3,7 @@ package co.eci.protocols;
 import static co.eci.protocols.exercices.URLs.methods;
 
 public class main {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         methods();
     }
 
